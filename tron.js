dbf0e1ff84bf7a8c783ccb0cabc0e9396dcb3f34
Ricardo Monteiro <71653983+m0nte1ro@users.noreply.github.com>
2020-11-13 00:40:29 +0000

    Very small bug fix
    The second grid wasnt big enough to create a "cross effect" on the right hand side of the canvas. This fix does it. *Line 92 & 93*. First time doing this btw, dont hate me.

--- tron.js
+++ tron.js
@@ -89,8 +89,8 @@ function drawBackground() {
 
   context.strokeStyle = '#000000';
   context.lineWidth = 2;
-  for (let i = 1; i <= canvas.width / unit; i += 2) {
-    for (let j = 1; j <= canvas.height / unit; j += 2) {
+  for (let i = 1; i <= (canvas.width+unit) / unit; i += 2) {
+    for (let j = 1; j <= (canvas.height+unit) / unit; j += 2) {
       context.strokeRect(0, 0, unit * i, unit * j);
     };
   };
